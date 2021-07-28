de1073eab7922c1481f355f267d1d94121551975
Conrad39 <conrad_vl@mail.ru>
2021-07-29 00:00:00 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    private final static UserService userService = new UserServiceImpl();
+    public static void main(String[] args)  {
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 35);
+        userService.saveUser("Сергей", "Петров", (byte) 42);
+        userService.saveUser("Марина", "Васильева", (byte) 23);
+        userService.saveUser("Игорь", "Семенов", (byte) 44);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,51 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+    private static Util instance = null;
+    private Util() {
+        try {
+            if (connection == null || connection.isClosed()){
+                Properties properties = getProps();
+                connection = DriverManager.getConnection(properties.getProperty("db.url"),
+                        properties.getProperty("db.username"), properties.getProperty("db.password"));
+            }
+        } catch (IOException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static Util getInstance(){
+        if (instance == null){
+            instance = new Util();
+        }
+        return instance;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+
+    private static Properties getProps() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream in = Files.newInputStream(Paths.get(Objects.requireNonNull(Util.class.getResource("/database.properties")).toURI()))) {
+            properties.load(in);
+            return properties;
+        } catch (IOException | URISyntaxException u){
+            throw new IOException("Base config not found", u);
+        }
+
+
+    }
+
+
 }
