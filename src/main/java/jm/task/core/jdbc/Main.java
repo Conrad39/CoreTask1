a79bc8ffb546bb1a272635ba9b54d069cca22f49
Conrad39 <conrad_vl@mail.ru>
2021-08-01 13:58:54 +0300

    Core JDBC Task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    private final static UserService userService = new UserServiceImpl();
+    public static void main(String[] args)  {
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 35);
+        userService.saveUser("Петр", "Петров", (byte) 42);
+        userService.saveUser("Сергей", "Сергеев", (byte) 23);
+        userService.saveUser("Николай", "Николаев", (byte) 44);
+
+        userService.removeUserById(3);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
